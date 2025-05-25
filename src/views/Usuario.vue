9f02afac870788d0e27a40dc0092b46c899d0d17
LEMMEL <LEMMEL@LEMMEL>
2025-05-25 18:07:16 -0500

    Ajuste Ususario

--- src/views/Usuario.vue
+++ src/views/Usuario.vue
@@ -79,7 +79,7 @@ export default {
       axios
         .get("http://127.0.0.1:8000/api/users")
         .then((response) => {
-          this.users = response.data.data ?? [];
+          this.users = response.data ?? [];
         })
         .catch((error) => {
           console.error("Error al cargar usuarios:", error);
